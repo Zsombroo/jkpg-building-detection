728b5741e32a6c022d07a71466b21014e39711ae
Zsombor Toth <toth.zsombor0920@gmail.com>
2021-05-11 18:55:17 +0200

    Add image slicer algorithm

---
+++ image_slicer.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import os
+import re
+
+
+def slice_image(
+    image: np.ndarray,
+    image_prefix: str, 
+    slice_size: tuple = (0, 0),
+    offset_h_w: tuple = (0, 0)
+    ) -> dict:
+
+    out = dict()
+    h, w = image.shape[:2]
+    for slice_y in range(int((h-offset_h_w[0])/slice_size[0])):
+        for slice_x in range(int((w-offset_h_w[1])/slice_size[1])):
+            out['{}_{}_{}'.format(image_prefix, slice_y, slice_x)] = \
+                image[offset_h_w[0]+slice_y*slice_size[0]:
+                      offset_h_w[0]+slice_y*slice_size[0]+slice_size[0],
+                      offset_h_w[1]+slice_x*slice_size[1]:
+                      offset_h_w[1]+slice_x*slice_size[1]+slice_size[1]]
+    return out
+    
+
+if __name__=='__main__':
+    SOURCE_PATH = './ortofoto'
+    DESTINATION_PATH = './sliced_raw'
+    SLICE_SIZE = (500, 500)
+
+    pattern = re.compile('.*\.tif$')
+    for file in sorted(os.listdir(SOURCE_PATH)):
+        if pattern.match(file):
+            slices = slice_image(cv2.imread('{}/{}'.format(SOURCE_PATH, file)),
+                                 file[:-4],
+                                 SLICE_SIZE)
+                                 
+            for k, v in slices.items():
+                cv2.imwrite('{}/{}.png'.format(DESTINATION_PATH, k), v)
