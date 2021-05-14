4ab5c7712509af574a79a23d9782d4cd2a955ae6
Zsombor Toth <toth.zsombor0920@gmail.com>
2021-05-14 11:03:48 +0200

    Minor modifications

--- canny_edge_detection.py
+++ canny_edge_detection.py
@@ -4,10 +4,14 @@ import os
 
 if __name__=='__main__':
     SOURCE_PATH = '../sliced_raw'
-    DESTINATION_PATH = '../canny_edges/low_th_170'
+    DESTINATION_PATH = '../canny_edges'
+    LOW_THRESHOLD, HIGH_THRESHOLD = 250, 255
 
     for image_file in sorted(os.listdir(SOURCE_PATH)):
         img = cv2.imread('{}/{}'.format(SOURCE_PATH, image_file),
                          cv2.IMREAD_GRAYSCALE)
-        edges = cv2.Canny(img, 170, 256)
-        cv2.imwrite('{}/{}'.format(DESTINATION_PATH, image_file), edges)
+        edges = cv2.Canny(img, LOW_THRESHOLD, HIGH_THRESHOLD)
+        cv2.imwrite('{}/{}_{}_{}.png'.format(DESTINATION_PATH, image_file[:-4],
+                    LOW_THRESHOLD,
+                    HIGH_THRESHOLD),
+                    edges)
