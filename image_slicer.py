e772a82fc48c97400b8e8654f3c017c3358cb5ea
Zsombor Toth <toth.zsombor0920@gmail.com>
2021-05-18 17:41:27 +0200

    Add configuration support to image_slicer.py

--- image_slicer.py
+++ image_slicer.py
@@ -1,3 +1,4 @@
+import configparser
 import cv2
 import numpy as np
 import os
@@ -7,35 +8,40 @@ import re
 def slice_image(
     image: np.ndarray,
     image_prefix: str, 
-    slice_size: tuple = (0, 0),
+    slice_size: int,
     offset_h_w: tuple = (0, 0)
     ) -> dict:
 
     out = dict()
     h, w = image.shape[:2]
-    for slice_y in range(int((h-offset_h_w[0])/slice_size[0])):
-        for slice_x in range(int((w-offset_h_w[1])/slice_size[1])):
+    for slice_y in range(int((h-offset_h_w[0])/slice_size)):
+        for slice_x in range(int((w-offset_h_w[1])/slice_size)):
             out['{}_{}_{}'.format(image_prefix, slice_y, slice_x)] = \
-                image[offset_h_w[0]+slice_y*slice_size[0]:
-                      offset_h_w[0]+slice_y*slice_size[0]+slice_size[0],
-                      offset_h_w[1]+slice_x*slice_size[1]:
-                      offset_h_w[1]+slice_x*slice_size[1]+slice_size[1]]
+                image[offset_h_w[0]+slice_y*slice_size:
+                      offset_h_w[0]+slice_y*slice_size+slice_size,
+                      offset_h_w[1]+slice_x*slice_size:
+                      offset_h_w[1]+slice_x*slice_size+slice_size]
     return out
     
 
 if __name__=='__main__':
-    SOURCE_PATH = '../ortofoto'
+    config = configparser.ConfigParser()
+    config.read('preprocessing.config')
+    slicer_condig = config['IMAGE_SLICER']
+
+    source_path = slicer_condig['source_path']
+    destination_path = slicer_condig['destination_path_without_size']
+    slice_size = int(slicer_condig['size'])
+    destination_path = '_'.join((destination_path, str(slice_size)))
+
+    if not os.path.isdir(destination_path):
+        os.mkdir(destination_path)
 
     pattern = re.compile('.*\.tif$')
-    for slice_one_side in [500, 640, 1024]:
-        DESTINATION_PATH = '../sliced_raw_{}_{}'.format(slice_one_side, slice_one_side)
-        SLICE_SIZE = (slice_one_side, slice_one_side)
-        
-        for file in sorted(os.listdir(SOURCE_PATH)):
-            if pattern.match(file):
-                slices = slice_image(cv2.imread('{}/{}'.format(SOURCE_PATH, file)),
-                                    file[:-4],
-                                    SLICE_SIZE)
-                                    
-                for k, v in slices.items():
-                    cv2.imwrite('{}/{}.png'.format(DESTINATION_PATH, k), v)
+    for file in sorted(os.listdir(source_path)):
+        if pattern.match(file):
+            slices = slice_image(cv2.imread('{}/{}'.format(source_path, file)),
+                                 file[:-4],
+                                 slice_size)
+            for k, v in slices.items():
+                cv2.imwrite('{}/{}.png'.format(destination_path, k), v)
